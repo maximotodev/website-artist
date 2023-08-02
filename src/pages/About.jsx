82be34c9c756a033841c972a19726dbec55f7cdc
maximotodev <maximotodev@proton.me>
2023-08-02 17:12:42 -0400

    imported zoom component and added it to homepage

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,10 @@
 import { useOutletContext } from "react-router-dom";
 import './About.css'
-import Accordion from 'react-bootstrap/Accordion';
 import SubscriptionBtn from './SubscriptionBtn'
 
 const About = () => {
   
   const { artistPhotos: [artistPhotos, setArtistPhotos]} = useOutletContext()
-
-  const accordionStyles = {backgroundColor: '#1f1d1d', color: 'white'}
 
   return (
     <div className="about-me-page">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import videoBg from '../assets/alyssa-grey-video2.mp4'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faInstagram, faTiktok, faSpotify, faYoutube } from '@fortawesome/free-brands-svg-icons'
 import { faVolumeHigh, faVolumeOff } from '@fortawesome/free-solid-svg-icons'
-import { AttentionSeeker } from "react-awesome-reveal";
+import { Zoom } from "react-awesome-reveal";
 
 import { useOutletContext } from "react-router-dom"
 
@@ -18,14 +18,21 @@ const Home = () => {
     <div className="home">
         <div className="overlay"></div>
         <video className='main-video' src={videoBg} autoPlay loop muted={isMute} playsInline />
-        <AttentionSeeker effect="flash" className="content">
+
+        <Zoom
+          duration={1200}>
+
           <h1 className='artist-logo' to='about'>A<span className="highlight-logo">lyssa</span><br></br> G<span className="highlight-logo">rey</span></h1>
+
           <div className="social-media">
-            <a target='_blank' rel='noreferrer' href="https://www.instagram.com/_alyssagrey/">
+            <Link to='https://www.instagram.com/_alyssagrey/'>
               <FontAwesomeIcon icon={faInstagram}/>
-            </a>
-            <a target='_blank' rel='noreferrer' href="https://www.tiktok.com/@_alyssagrey">
+            </Link>
+            <Link to='https://www.tiktok.com/@_alyssagrey'>
               <FontAwesomeIcon icon={faTiktok}/>
+            </Link>
+            <a onClick={toggleMute} href="#">
+            <FontAwesomeIcon icon={ isMute ? faVolumeOff : faVolumeHigh } />
             </a>
             <a target='_blank' rel='noreferrer' href="https://open.spotify.com/artist/3pepeKuCVRstCagyJZrGfq">
               <FontAwesomeIcon icon={faSpotify}/>
@@ -33,11 +40,8 @@ const Home = () => {
             <a target='_blank' rel='noreferrer' href="https://www.youtube.com/@alyssagrey6324">
               <FontAwesomeIcon icon={faYoutube}/>
             </a>
-            <a onClick={toggleMute} href="#">
-            <FontAwesomeIcon icon={ isMute ? faVolumeOff : faVolumeHigh } />
-            </a>
           </div>
-        </AttentionSeeker>
+        </Zoom>
     </div>
   )
 };
